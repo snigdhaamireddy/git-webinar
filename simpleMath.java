0baf5ee1b0a0feebb96dd4cb82af6799d43ccb96
Amireddy Snigdha <snigdha.amireddy@zemosolabs.com>
2022-12-23 16:25:50 +0530

    Simple Math calculations are added in .java file

---
+++ simpleMath.java
@@ -0,0 +1,11 @@
+class SimpleMath
+{
+	public static void main(String args[])
+	{
+		int a=10,b=5;
+		System.out.println(a+"+"+b+"="+(a+b));
+		System.out.println(a+"-"+b+"="+(a-b));
+		System.out.println(a+"*"+b+"="+(a*b));
+		System.out.println(a+"/"+n+"="+(a/b));
+	}
+}
